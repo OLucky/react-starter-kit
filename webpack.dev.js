902d308902c9d7bcb25893d0cdec5425e2d98cf9
Georgii Ishchenko <ge.ishchenko@gmail.com>
2018-06-29 15:14:28 +0300

    Changelog 1. Add support for page templates with html-webpack-plugin and html-loader 2. Refactor webpack config with webpack-merge 3. Ensure hot-reloading on change in styles 4. Add static folder

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,64 +1,19 @@
 const path = require("path");
-const webpack = require("webpack");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-  context: path.resolve(__dirname, "./src"),
-  entry: ["./index.js"],
   mode: "development",
   output: {
-    path: path.resolve(__dirname, "./src/public"),
-    filename: "main.js",
+    path: path.resolve(__dirname, "serve"),
     publicPath: ""
   },
-  module: {
-    rules: [{
-      test: /\.js$/,
-      loader: "babel-loader",
-      options: {
-        cacheDirectory: true
-      },
-      include: [path.resolve(__dirname, "src")],
-      exclude: /node_modules/
-    },
-    {
-      test: /\.css$/,
-      use: ["style-loader", "css-loader"]
-    },
-    {
-      test: /\.scss$/,
-      exclude: /node_modules/,
-      use: ["style-loader", "css-loader", "postcss-loader", "sass-loader"]
-    },
-    {
-      test: /\.(jpe?g|png|gif)$/i,
-      use: [{
-        loader: "file-loader",
-        options: {
-          name: "./[path][name].[ext]"          
-        }
-      }]
-    },
-    {
-      test: /\.(ttf|eot|svg|otf)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-      use: [{
-        loader: "file-loader",
-        options: {
-          name: "./[path][name].[ext]"
-        }
-      }]
-    }
-    ]
-  },
   devtool: "source-map",
-  target: "web",
   plugins: [
-    new webpack.DefinePlugin({
-      "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV)
-    })
+    new CopyWebpackPlugin([{from: path.join(__dirname, "src/static"), to: path.join(__dirname, "serve")}]),
   ],
   serve: {
     port: 9000,
-    content: [path.join(__dirname, "./src/public")],
+    content: [path.join(__dirname, "serve")],
     hot: {
       hot: true
     }
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,65 +1,17 @@
 const path = require("path");
-const webpack = require("webpack");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
 const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
-module.exports = env => ({
-  context: path.resolve(__dirname, "./src"),
-  entry: ["./index.js"],
+module.exports = {
   mode: "production",
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "main.js",
-    publicPath: "",
-    hotUpdateChunkFilename: "../hot/hot-update.js",
-    hotUpdateMainFilename: "../hot/hot-update.json"
+    publicPath: ""
   },
-  module: {
-    rules: [{
-      test: /\.js$/,
-      loader: "babel-loader",
-      include: [path.resolve(__dirname, "src")],
-      exclude: /node_modules/
-    },
-    {
-      test: /\.css$/,
-      use: [MiniCssExtractPlugin.loader, "css-loader"]
-    },
-    {
-      test: /\.scss$/,
-      exclude: /node_modules/,
-      use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader", "sass-loader"]
-    },
-    {
-      test: /\.(jpe?g|png|gif)$/i,
-      use: [{
-        loader: "file-loader",
-        options: {
-          name: "./[path][name].[ext]"
-        }
-      }]
-    },
-    {
-      test: /\.(ttf|eot|svg|otf)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-      use: [{
-        loader: "file-loader",
-        options: {
-          name: "./[path][name].[ext]"
-        }
-      }]
-    }
-    ]
-  },
-  target: "web",
   plugins: [
-    new webpack.DefinePlugin({
-      "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV)
-    }),
-    new CopyWebpackPlugin([{from: path.join(__dirname, "/src/public"), to: path.join(__dirname, "dist")}]),
-    new MiniCssExtractPlugin({
-      filename: "styles.css"
-    }),
-    new BundleAnalyzerPlugin({analyzerMode: env.ANALYZE_BUNDLE ? "server" : "disabled"})
+    new CleanWebpackPlugin(["dist"]),
+    new CopyWebpackPlugin([{from: path.join(__dirname, "src/static"), to: path.join(__dirname, "dist")}]),
+    new BundleAnalyzerPlugin({analyzerMode: process.env.ANALYZE_BUNDLE ? "server" : "disabled"})
   ]
-});
+};
